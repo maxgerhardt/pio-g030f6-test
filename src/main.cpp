8ac3665d1a2aeaaa5353d597f6306dd09c67b1e2
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2021-11-19 12:46:09 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h> 
 
+#ifdef ARDUINO_GENERIC_G030F6PX
+/* for G0 board test */
 #define BLINKY_LED PB7
+#else
+/* for F0 board test */
+#define BLINKY_LED PB1
+#endif
 
 void setup() {
    Serial.begin(9600); /* generic variant: TX = PA2, RX = PA3 */
@@ -14,7 +20,8 @@ void loop() {
    delay(500);
 }
 
-/* addition: instead of generic reset clock (8MHz), use HSI+PLL to 64Mhz */
+/* addition for G0 board: instead of generic reset clock (8MHz), use HSI+PLL to 64Mhz */
+#if defined(ARDUINO_GENERIC_G030F6PX)
 extern "C" void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -53,4 +60,40 @@ extern "C" void SystemClock_Config(void)
     Error_Handler();
   }
 }
+#endif
 
+/* addition for F0 board: instead of generic reset clock (8MHz), use HSI+PLL to 48Mhz */
+#if defined(ARDUINO_GENERIC_F031F6PX)
+extern "C" void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+#endif 
